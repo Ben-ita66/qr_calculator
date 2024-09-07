9582c9a472386448adba497b091d33e938fe81f7
Ben-ita66 <benitanwabueze72@gmail.com>
2024-09-07 22:23:53 +0200

    first commit

---
+++ dummy.ts
@@ -0,0 +1,11 @@
+function myFunc(name:string,age:number){
+    //execution
+    console.log('Hi, ' + name + ' How are you doing')
+    console.log(`Hi, ${name} how are you doing`)
+}
+
+
+
+
+
+myFunction('Samuel')
